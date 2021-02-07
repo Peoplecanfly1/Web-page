e37f07938da9b707377f2295923d709a44c24115
Yury <Yura388@yandex.ru>
2021-02-07 13:17:26 +0300

    finished first page

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+ let sidemenu = document.querySelector('.hide')
+ let sidemenu_list = document.querySelector('.hidden-list')
+ console.log(sidemenu, sidemenu_list)
+
+ addEventListener(sidemenu, onclick,);
